07ab27e74d56a0e6cf841259adea782bd64bcd80
Lucas Barros <lcsavb@gmail.com>
2024-03-24 08:38:48 +0100

    functions to check overflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,17 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    let mut steps = 0;
 
     if n == 0 {
         return None;
     }
     
+    let mut steps = 0;
     let mut current = n;
 
     while current != 1 {
         if current % 2 == 0 {
             current /= 2;
         } else {
-            if current > (std::u64::MAX - 1) / 3 {
-                return None;
-            }
-            current = 3 * current + 1;
+            current = current.checked_mul(3)?.checked_add(1)?;
         }
         steps += 1;
     }
